46a21f35f60a6e2da33f02e079d1c4c87066df4c
KrokoNinja <79910460+KrokoNinja@users.noreply.github.com>
2022-10-18 19:52:10 +0200

    Finished Task 2.4

--- src/Student.java
+++ src/Student.java
@@ -1,11 +1,23 @@
 import java.util.Date;
 
 public class Student {
-    String firstname;
-    String lastname;
-    long studentId;
-    double weight;
-    Date birthday;
+    private static String firstname;
+    private static String lastname;
+    private static long studentId;
+    private static double weight;
+    private static Date birthday;
 
     public enum SortKey {FIRSTNAME, LASTNAME, STUDENT_ID, WEIGHT, BIRTHDAY}
+
+    Student() {
+        this(null, null, studentId, 0, new Date(0));
+    }
+
+    Student(String firstname, String lastname, long studentId, double weight, Date birthday){
+        Student.firstname = firstname;
+        Student.lastname = lastname;
+        Student.studentId = studentId;
+        Student.weight = weight;
+        Student.birthday = birthday;
+    }
 }

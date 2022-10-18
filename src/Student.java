6adfe3fca3c33471a88174fdb6b70a3ed449a4d8
KrokoNinja <79910460+KrokoNinja@users.noreply.github.com>
2022-10-18 20:06:16 +0200

    Finished Task 2.5

--- src/Student.java
+++ src/Student.java
@@ -13,11 +13,51 @@ public class Student {
         this(null, null, studentId, 0, new Date(0));
     }
 
-    Student(String firstname, String lastname, long studentId, double weight, Date birthday){
+    Student(String firstname, String lastname, long studentId, double weight, Date birthday) {
         Student.firstname = firstname;
         Student.lastname = lastname;
         Student.studentId = studentId;
         Student.weight = weight;
+        Student.birthday = birthday;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public static void setFirstname(String firstname) {
+        Student.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public static void setLastname(String lastname) {
+        Student.lastname = lastname;
+    }
+
+    public static String getName() {
+        return firstname + " " + lastname;
+    }
+
+    public long getStudentId() {
+        return studentId;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public static void setWeight(double weight) {
+        Student.weight = weight;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public static void setBirthday(Date birthday) {
         Student.birthday = birthday;
     }
 }
